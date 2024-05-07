0394c2711ff2bea6c95ea1abaf3db88a9a87ab54
adrianlamdev <me@adrianlam.dev>
2024-05-07 10:21:22 -0700

    feat: add jotai + provider #5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
-import Image from "next/image";
+"use client";
+
+import { useAtom, atom } from "jotai";
 
 export default function Home() {
+  const testAtom = atom([
+    {
+      title: "Ghost in the Shell",
+      year: 1995,
+      watched: true,
+    },
+    {
+      title: "Serial Experiments Lain",
+      year: 1998,
+      watched: false,
+    },
+  ]);
+  const [test, setTest] = useAtom(testAtom);
   return <main></main>;
 }
