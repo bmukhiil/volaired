0b8ed3357a91e998d6d9e4e264a49e5f20652e83
adrianlamdev <me@adrianlam.dev>
2024-05-20 21:35:53 -0700

    fix: add id to script #16

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,21 +2,6 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Script from "next/script";
 
-const jsonLd = {
-  "@context": "https://schema.org",
-  "@type": "WebPage",
-  url: "https://volaired.com/not-found",
-  name: "404 - Page Not Found - Volaired",
-  description: "The page you are looking for does not exist on Volaired.",
-  // publisher: {
-  //   "@type": "Organization",
-  //   name: "Volaired",
-  //   logo: {
-  //     "@type": "ImageObject",
-  //     url: "https://volaired.com/images/logo.png",
-  //   },
-  // },
-};
 
 export const metadata = {
   title: "404 - Page Not Found - Volaired",
@@ -30,10 +15,6 @@ export const metadata = {
 export default function NotFound() {
   return (
     <main className="px-6 w-screen h-[92dvh] flex justify-center flex-col gap-y-5 overflow-hidden">
-      <Script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-      />
       <div className="">
         <h1 className="text-lg text-indigo-500 font-semibold tracking-tight">
           404
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,6 +109,7 @@ export default function Home() {
       id="hero"
     >
       <Script
+        id="json-ld-schema"
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
