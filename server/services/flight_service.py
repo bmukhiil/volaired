bad3b5d3e98e7b748741e7fa2112569aa74d4c6c
mukhiil <bmukhiil@gmail.com>
2024-05-24 05:26:10 -0700

    add amadeus client to flight_service

---
+++ server/services/flight_service.py
@@ -0,0 +1,7 @@
+from amadeus import Client, Location, ResponseError
+
+#use this function to access amadeus calls - look at this link: https://pypi.org/project/amadeus/
+amadeus = Client(
+    client_id='cRtVjhwJYi2zr8MHE7adjhBEh4hYgGKA',
+    client_secret='z4Zdp26tfwFrwBmA'
+)
