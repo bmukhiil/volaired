7438c4b2247925d59567911ccd9b0aa6a64b672f
mukhiil <bmukhiil@gmail.com>
2024-05-25 04:17:52 -0700

    add response/request data + sample flight search working

--- server/api/flights.py
+++ server/api/flights.py
@@ -1,9 +1,14 @@
 from flask_restx import Resource
+from flask import request
+from services.flight_service import flight_search
 
 class FlightsSearch(Resource):
 	def post(self):
    # Logic to search for flights based on criteria
-		return {'message': 'Searching for flights...'}
+		request_data = request.json
+		r_data = flight_search(request_data)
+  
+		return r_data
 
 class FlightsAddToPlan(Resource):
 	def post(self):
--- server/services/flight_service.py
+++ server/services/flight_service.py
@@ -1,7 +1,71 @@
 from amadeus import Client, Location, ResponseError
+from flask import jsonify
 
 #use this function to access amadeus calls - look at this link: https://pypi.org/project/amadeus/
 amadeus = Client(
     client_id='cRtVjhwJYi2zr8MHE7adjhBEh4hYgGKA',
     client_secret='z4Zdp26tfwFrwBmA'
 )
+
+def flight_search(request):
+    dd = request["departureDate"]
+    rd = request["returnDate"]
+    t = request["travelers"]
+    
+    body = {
+        "currencyCode": "USD",
+        "originDestinations": [
+            {
+                "id": "1",
+                "originLocationCode": "NYC",
+                "destinationLocationCode": "MAD",
+                "departureDateTimeRange": {
+                    "date": "2024-11-01",
+                    "time": "10:00:00"
+                }
+            },
+                       {
+                "id": "2",
+                "originLocationCode": "MAD",
+                "destinationLocationCode": "NYC",
+                "departureDateTimeRange": {
+                    "date": "2024-11-04",
+                    "time": "10:00:00"
+                }
+            }
+        ],
+        "travelers": [
+            {
+                "id": "1",
+                "travelerType": "ADULT"
+            }
+        ],
+        "sources": [
+            "GDS"
+        ],
+        "searchCriteria": {
+            "maxFlightOffers": 2,
+            "flightFilters": {
+                "cabinRestrictions": [
+                    {
+                        "cabin": "BUSINESS",
+                        "coverage": "MOST_SEGMENTS",
+                        "originDestinationIds": [
+                            "1"
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+    
+    body["originDestinations"][0]["departureDateTimeRange"]["date"] = dd
+    body["originDestinations"][1]["departureDateTimeRange"]["date"] = rd
+    
+    flights = amadeus.shopping.flight_offers_search.post(body)
+    
+
+    
+    
+    return jsonify(flights.data)
+    
