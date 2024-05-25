5c880acd2a595ca113e799066d8ddcde08efa703
mukhiil <bmukhiil@gmail.com>
2024-05-25 16:11:01 -0700

    flight-search working, need to add travellers as well

--- server/services/flight_service.py
+++ server/services/flight_service.py
@@ -1,5 +1,7 @@
 from amadeus import Client, Location, ResponseError
 from flask import jsonify
+import json
+
 
 #use this function to access amadeus calls - look at this link: https://pypi.org/project/amadeus/
 amadeus = Client(
@@ -7,10 +9,16 @@ amadeus = Client(
     client_secret='z4Zdp26tfwFrwBmA'
 )
 
+
+
 def flight_search(request):
-    dd = request["departureDate"]
-    rd = request["returnDate"]
-    t = request["travelers"]
+    departureDate = request["departureDate"]
+    returnDate = request["returnDate"]
+    travelers = request["travelers"]
+    startCity = request["startCity"]
+    endCity = request["endCity"]
+    
+    
     
     body = {
         "currencyCode": "USD",
@@ -44,7 +52,7 @@ def flight_search(request):
             "GDS"
         ],
         "searchCriteria": {
-            "maxFlightOffers": 2,
+            "maxFlightOffers":4,
             "flightFilters": {
                 "cabinRestrictions": [
                     {
@@ -59,13 +67,52 @@ def flight_search(request):
         }
     }
     
-    body["originDestinations"][0]["departureDateTimeRange"]["date"] = dd
-    body["originDestinations"][1]["departureDateTimeRange"]["date"] = rd
+    body["originDestinations"][0]["departureDateTimeRange"]["date"] = departureDate
+    body["originDestinations"][1]["departureDateTimeRange"]["date"] = returnDate
+    body["originDestinations"][0]["originLocationCode"] = startCity
+    body["originDestinations"][0]["destinationLocationCode"] = endCity
+    body["originDestinations"][1]["originLocationCode"] = endCity
+    body["originDestinations"][1]["destinationLocationCode"] = startCity
+    
+
+        
     
     flights = amadeus.shopping.flight_offers_search.post(body)
+    
+    
+    formatted_data = []
+    
+    flights = flights.data
+
+    for item in flights:
+        formatted_item = {
+            "id": int(item["id"]),
+            "outboundTotalDuration": item["itineraries"][0]["duration"],
+            "inboundTotalDuration": item["itineraries"][1]["duration"],
+            "flightPath": [],
+            "price": float(item["price"]["total"])
+        }
+
+        for itinerary in item["itineraries"]:
+            for segment in itinerary["segments"]:
+                formatted_segment = {
+                    "departureAirport": segment["departure"]["iataCode"],
+                    "departureTime": segment["departure"]["at"],
+                    "arrivalAirport": segment["arrival"]["iataCode"],
+                    "arrivalTime": segment["arrival"]["at"],
+                    "airlines": segment["carrierCode"],
+                    "Duration": segment["duration"]
+                }
+                formatted_item["flightPath"].append(formatted_segment)
+
+        formatted_data.append(formatted_item)
+
+    # Printing the formatted data
+    print(json.dumps(formatted_data, indent=2))
+    
     
 
     
     
-    return jsonify(flights.data)
+    return jsonify(formatted_data)
     
