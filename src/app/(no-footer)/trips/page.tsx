8e9ef3b4938a01d49879ccfaf3183699a796f531
mohithbaskaran <mbaska01@student.ubc.ca>
2024-06-01 19:38:51 -0700

    sidebar + trips pg  added

---
+++ src/app/(no-footer)/trips/page.tsx
@@ -0,0 +1,11 @@
+import { Sidebar } from "@/components/layout/sidebar";
+
+export default function TripsPage() {
+  return (
+    <div className="h-screen -mx-6 -my-6 flex bg-gray-100">
+      <Sidebar />
+      <div className="flex-1 p-4">
+      </div>
+    </div>
+  );
+}
